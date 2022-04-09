2542a470df861317a8c4563ccc7f2239b2ac650e
David Alejandro Ijaji <dijaji@bancolombia.com.co>
2022-04-08 23:16:06 -0500

    feature(mongo): Code for users and appoinments

--- flask-backend-mongo/api/app.py
+++ flask-backend-mongo/api/app.py
@@ -1,3 +1,4 @@
+from urllib import response
 from flask import Flask, request, jsonify
 from bson.json_util import dumps
 from bson.objectid import ObjectId
@@ -6,32 +7,32 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+######################################## CONSULTAS CITAS MEDICAS #################################################
 
-@app.route("/movies/<code>", methods=['GET'])
-def get_movie(code):
+
+#crea una cita medica
+@app.route("/citas", methods=['POST'])
+def create_appoinment():
+    data = request.get_json()
     con = db.get_connection()
-    dbmov = con.dbmovies
+    dbAppoinment = con.dbAppoinment 
     try:
-        movies = dbmov.movies
-        response = app.response_class(
-            response=dumps(movies.find_one({'_id': ObjectId(code)})),
-            status=200,
-            mimetype='application/json'
-        )
-        return response
+        medAppoinment = dbAppoinment.medAppoinment
+        medAppoinment.insert_one(data)
+        return jsonify({"message":"OK"})
     finally:
         con.close()
         print("Connection closed")
 
-@app.route("/movies", methods=['GET'])
-def get_movies():
+@app.route("/citas", methods=['GET'])
+def get_all_appoinments():
     con = db.get_connection()
-    dbmov = con.dbmovies
+    dbAppoinment = con.dbAppoinment 
     try:
-        movies = dbmov.movies
+        medAppoinment = dbAppoinment.medAppoinment
         response = app.response_class(
             response=dumps(
-                movies.find()
+                medAppoinment.find()
             ),
             status=200,
             mimetype='application/json'
@@ -41,27 +42,33 @@ def get_movies():
         con.close()
         print("Connection closed")
 
-@app.route("/movies", methods=['POST'])
-def create():
-    data = request.get_json()
+
+#lee una cita medica por id de la cita
+@app.route("/citas/<code>", methods=['GET'])
+def get_appoinment(code):
     con = db.get_connection()
-    dbmov = con.dbmovies  
+    dbAppoinment = con.dbAppoinment 
     try:
-        movies = dbmov.movies
-        movies.insert_one(data)
-        return jsonify({"message":"OK"})
+        medAppoinment = dbAppoinment.medAppoinment
+        response = app.response_class(
+            response=dumps(medAppoinment.find_one({'_id': ObjectId(code)})),
+            status=200,
+            mimetype='application/json'
+        )
+        return response
     finally:
         con.close()
         print("Connection closed")
 
-@app.route("/movies/<code>", methods=['PUT'])
-def update(code):
+#Modifica una cita medica
+@app.route("/citas/<code>", methods=['PUT'])
+def update_appoinment(code):
     data = request.get_json()
     con = db.get_connection()
-    dbmov = con.dbmovies
+    dbAppoinment = con.dbAppoinment
     try:
-        movies = dbmov.movies
-        movies.replace_one(
+        medAppoinment = dbAppoinment.medAppoinment
+        medAppoinment.replace_one(
             {'_id': ObjectId(code)},
             data, True
         )
@@ -70,13 +77,92 @@ def update(code):
         con.close()
         print("Connection closed")
 
-@app.route("/movies/<code>", methods=['DELETE'])
-def delete(code):
+#Cancela una cita medica
+@app.route("/citas/<code>", methods=['DELETE'])
+def delete_appoinment(code):
     con = db.get_connection()
-    dbmov = con.dbmovies
+    dbAppoinment = con.dbAppoinment
     try:
-        movies = dbmov.movies
-        movies.delete_one({'_id': ObjectId(code)})
+        medAppoinment = dbAppoinment.medAppoinment
+        medAppoinment.delete_one({'_id': ObjectId(code)})
+        return jsonify({"message":"OK"})
+    finally:
+        con.close()
+        print("Connection closed")
+
+
+######################################## CONSULTAS DE PACIENTES #################################################
+
+@app.route("/pacientes", methods=['GET'])
+def get_all_users():
+    con = db.get_connection()
+    usuarios = con.usuarios
+    try:
+        pacientes = usuarios.pacientes
+        response = app.response_class(
+            response=dumps(
+                pacientes.find()
+            ),
+            status=200,
+            mimetype='application/json'
+        )
+        return response
+    finally:
+        con.close()
+        print("Connection closed")
+
+@app.route("/pacientes/<code>", methods=['GET'])
+def get_user(code):
+    con = db.get_connection()
+    usuarios = con.usuarios
+    try:
+        pacientes = usuarios.pacientes
+        response = app.response_class(
+            response=dumps(pacientes.find_one({'_id': ObjectId(code)})),
+            status=200,
+            mimetype='application/json'
+        )
+        return response
+    finally:
+        con.close()
+        print("Connection closed")
+
+@app.route("/pacientes", methods=['POST'])
+def create_user():
+    data = request.get_json()
+    con = db.get_connection()
+    usuarios = con.usuarios  
+    try:
+        pacientes = usuarios.pacientes
+        pacientes.insert_one(data)
+        return jsonify({"message":"OK"})
+    finally:
+        con.close()
+        print("Connection closed")
+
+@app.route("/pacientes/<code>", methods=['PUT'])
+def update_user(code):
+    data = request.get_json()
+    con = db.get_connection()
+    usuarios = con.usuarios
+    try:
+        pacientes = usuarios.pacientes
+        pacientes.replace_one(
+            {'_id': ObjectId(code)},
+            data, True
+        )
+        return jsonify({"message":"OK"})
+    finally:
+        con.close()
+        print("Connection closed")
+
+@app.route("/pacientes/<code>", methods=['DELETE'])
+def delete_user(code):
+    con = db.get_connection()
+    usuarios = con.usuarios
+    try:
+        pacientes = usuarios.pacientes
+        pacientes.delete_one({'_id': ObjectId(code)})
         return jsonify({"message":"OK"})
     finally:
         con.close()
